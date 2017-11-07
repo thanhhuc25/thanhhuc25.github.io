16072f365c8ed30bfdbf44cc8eb49800cd3cb0cc
do <do@do.com>
2017-11-07 10:22:20 +0700

    ds

--- webqr.js
+++ webqr.js
@@ -103,11 +103,16 @@ function htmlEntities(str) {
 
 function read(a)
 {
-    var html="<br>";
-    if(a.indexOf("http://") === 0 || a.indexOf("https://") === 0)
-        html+="<a target='_blank' href='"+a+"'>"+a+"</a><br>";
-    html+="<b>"+htmlEntities(a)+"</b><br><br>";
-    document.getElementById("result").innerHTML=html;
+    // var html="<br>";
+    // if(a.indexOf("http://") === 0 || a.indexOf("https://") === 0)
+    //     html+="<a target='_blank' href='"+a+"'>"+a+"</a><br>";
+    // html+="<b>"+htmlEntities(a)+"</b><br><br>";
+    // document.getElementById("result").innerHTML=html;
+
+
+    if(a.indexOf("http://") === 0 || a.indexOf("https://") === 0){
+        window.location.href = a;
+    }
 }	
 
 function isCanvasSupported(){
